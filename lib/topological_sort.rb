6067d12fd9715837e4070a021e718837f63c8aa9
Tony <tonywzhang@gmail.com>
2018-09-11 16:06:27 -0700

    both solutions

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -4,48 +4,55 @@ require_relative 'graph'
 
 def topological_sort(vertices)
 
+  #khans algorithm
+  queue = []
   sorted = []
-  explored = Set.new
 
-  vertices.each do |vertex|
-    dfs(vertex, explored, sorted) if !explored.include?(vertex)
+  vertices.each do |vert|
+    queue.push(vert) if vert.in_edges.empty?
   end
 
-  sorted
-end
+  while !queue.empty?
+    current = queue.shift
+    sorted << current
 
-def dfs(vertex, explored, sorted)
-
-  explored.add(vertex)
-
-  vertex.out_edges.each do |edge|
-    new_vertex = edge.to_vertex
-    if !explored.include?(new_vertex)
-      dfs(new_vertex, explored, sorted)
+    current.out_edges.dup.each do |edge|
+      if edge.to_vertex.in_edges.count == 1
+        queue.push(edge.to_vertex)
+      end
+      edge.destroy!
     end
   end
 
-  sorted.unshift(vertex)
+  vertices.length == sorted.length ? sorted : []
+  
 end
-
-# #khans algorithm
-# queue = []
-# sorted = []
+#   sorted = []
+#   explored = Set.new
+#   this_stack = Set.new
+#   cycle = false
 #
-# vertices.each do |vert|
-#   queue.push(vert) if vert.in_edges.empty?
-# end
-#
-# while !queue.empty?
-#   current = queue.shift
-#   sorted << current
-#
-#   current.out_edges.dup.each do |edge|
-#     if edge.to_vertex.in_edges.count == 1
-#       queue.push(edge.to_vertex)
-#     end
-#     edge.destroy!
+#   vertices.each do |vertex|
+#     cycle = dfs(vertex, explored, sorted, this_stack, false) if !explored.include?(vertex)
+#     return [] if cycle
 #   end
+#
+#   sorted
 # end
 #
-# vertices.length == sorted.length ? sorted : []
+# def dfs(vertex, explored, sorted, this_stack, cycle)
+#   return true if this_stack.include?(vertex)
+#   explored.add(vertex)
+#   this_stack.add(vertex)
+#
+#   vertex.out_edges.each do |edge|
+#     new_vertex = edge.to_vertex
+#     if !explored.include?(new_vertex)
+#       cycle = dfs(new_vertex, explored, sorted, this_stack, false)
+#     end
+#     return true if cycle
+#   end
+#
+#   this_stack.delete(vertex)
+#   sorted.unshift(vertex)
+#   false
