a28cec4e1701ea531450c4c3d05cb5cfb51e0ac1
Tony <tonywzhang@gmail.com>
2018-09-11 15:55:53 -0700

    Tarjan's Algorithm

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -4,26 +4,48 @@ require_relative 'graph'
 
 def topological_sort(vertices)
 
-  #khans algorithm
-  queue = []
   sorted = []
+  explored = Set.new
 
-  vertices.each do |vert|
-    queue.push(vert) if vert.in_edges.empty?
+  vertices.each do |vertex|
+    dfs(vertex, explored, sorted) if !explored.include?(vertex)
   end
 
-  while !queue.empty?
-    current = queue.shift
-    sorted << current
+  sorted
+end
 
-    current.out_edges.dup.each do |edge|
-      if edge.to_vertex.in_edges.count == 1
-        queue.push(edge.to_vertex)
-      end
-      edge.destroy!
+def dfs(vertex, explored, sorted)
+
+  explored.add(vertex)
+
+  vertex.out_edges.each do |edge|
+    new_vertex = edge.to_vertex
+    if !explored.include?(new_vertex)
+      dfs(new_vertex, explored, sorted)
     end
   end
 
-  vertices.length == sorted.length ? sorted : []
-
+  sorted.unshift(vertex)
 end
+
+# #khans algorithm
+# queue = []
+# sorted = []
+#
+# vertices.each do |vert|
+#   queue.push(vert) if vert.in_edges.empty?
+# end
+#
+# while !queue.empty?
+#   current = queue.shift
+#   sorted << current
+#
+#   current.out_edges.dup.each do |edge|
+#     if edge.to_vertex.in_edges.count == 1
+#       queue.push(edge.to_vertex)
+#     end
+#     edge.destroy!
+#   end
+# end
+#
+# vertices.length == sorted.length ? sorted : []
