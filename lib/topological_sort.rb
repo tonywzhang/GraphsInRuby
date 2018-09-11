4b1abd2660f7561223cb308b8f8eb99aa36e853e
Tony <tonywzhang@gmail.com>
2018-09-11 15:37:50 -0700

    Khan's Algorithm

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -4,29 +4,26 @@ require_relative 'graph'
 
 def topological_sort(vertices)
 
-  
+  #khans algorithm
+  queue = []
+  sorted = []
+
+  vertices.each do |vert|
+    queue.push(vert) if vert.in_edges.empty?
+  end
+
+  while !queue.empty?
+    current = queue.shift
+    sorted << current
+
+    current.out_edges.dup.each do |edge|
+      if edge.to_vertex.in_edges.count == 1
+        queue.push(edge.to_vertex)
+      end
+      edge.destroy!
+    end
+  end
+
+  vertices.length == sorted.length ? sorted : []
 
 end
-
-
-
-# queue = []
-# sorted = []
-#
-# vertices.each do |vert|
-#   queue.push(vert) if vert.in_edges.empty?
-# end
-#
-# while !queue.empty?
-#   current = queue.shift
-#   sorted << current
-#
-#   current.out_edges.dup.each do |edge|
-#     if edge.to_vertex.in_edges.count == 1
-#       queue.push(edge.to_vertex)
-#     end
-#     edge.destroy!
-#   end
-# end
-#
-# vertices.length == sorted.length ? sorted : []
