66ed4fc751f80bf4c42f710c0a40f73ba9d38669
Tony <tonywzhang@gmail.com>
2018-09-12 09:27:44 -0700

    install order

--- lib/install_order.rb
+++ lib/install_order.rb
@@ -10,5 +10,25 @@
 
 
 def install_order(arr)
-  
+  max = 0
+  vertices = {}
+  arr.each do |tuple|
+    # create the graph
+    vertices[tuple[0]] = Vertex.new(tuple[0]) unless vertices[tuple[0]]
+    vertices[tuple[1]] = Vertex.new(tuple[1]) unless vertices[tuple[1]]
+    Edge.new(vertices[tuple[1]], vertices[tuple[0]])
+
+    #reset max if needed
+    max = tuple.max if tuple.max > max
+  end
+
+
+  # find the missing packages
+  independent = []
+  (1..max).each do |i|
+    independent << i unless vertices[i]
+  end
+
+  # sort the vertices of the graph and add the missing packages
+  independent + topological_sort(vertices.values).map { |v| v.value } 
 end
